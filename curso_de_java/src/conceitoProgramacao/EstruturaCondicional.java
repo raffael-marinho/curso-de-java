66f87f8a2f2b09a28ffb497f5514d51f2aaa7c99
raffael-marinho <silvamarinho10@gmail.com>
2024-12-30 18:47:39 -0300

    fix: alinhamento do codigo estrutura sequencial e condicional

--- curso_de_java/src/conceitoProgramacao/EstruturaCondicional.java
+++ curso_de_java/src/conceitoProgramacao/EstruturaCondicional.java
@@ -8,159 +8,144 @@ public class EstruturaCondicional {
 	public static void main(String[] args) {
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
-		
-		//Fazer um programa para ler um número inteiro, e depois dizer se este número é negativo ou não.
-		
-		/*System.out.println("digite o primeiro numero inteiro:");
-		int numero = sc.nextInt();
-		if (numero<0) 
-			System.out.println("NEGATIVO");
-		else
-			System.out.println("NAO NEGATIVO");*/
-		
-		//Fazer um programa para ler um número inteiro e dizer se este número é par ou ímpar.
-		
-		/*System.out.println("digite o primeiro numero inteiro:");
-		int numero = sc.nextInt();
-		if (numero%2==0) 
-			System.out.println("PAR");
-		else
-			System.out.println("IMPAR");*/
-		
-		/*Leia 2 valores inteiros (A e B). Após, o programa deve mostrar uma mensagem "Sao Multiplos" ou "Nao sao
-Multiplos", indicando se os valores lidos são múltiplos entre si. Atenção: os números devem poder ser digitados em
-ordem crescente ou decrescente*/
-		
-		/*System.out.println("digite o primeiro numero inteiro:");
-		int numero1 = sc.nextInt();
-		System.out.println("digite o segundo numero inteiro:");
-		int numero2 = sc.nextInt();
-		
-		if (numero2%numero1 == 0 || numero1%numero2 == 0) 
-			System.out.println("Sao Multiplos");
-		else
-			System.out.println("Nao sao Multiplos");*/
-		
-		/*Leia a hora inicial e a hora final de um jogo. A seguir calcule a duração do jogo, sabendo que o mesmo pode
-começar em um dia e terminar em outro, tendo uma duração mínima de 1 hora e máxima de 24 horas.*/
-		
-		/*System.out.println("digite a hora inicial do jogo:");
-		int horaInicial = sc.nextInt();
-		System.out.println("digite a hora final do jogo:");
-		int horaFinal = sc.nextInt();
-		int calculoHora;
-		if (horaInicial < horaFinal) 
-			calculoHora = horaFinal - horaInicial;
-		else
-			calculoHora = 24 - horaInicial + horaFinal;
-		
-		System.out.println("O JOGO DUROU " + calculoHora + " HORA(S)");*/
 
-		/*Com base na tabela abaixo, escreva um programa que leia o código de um item e a quantidade deste item. A
-seguir, calcule e mostre o valor da conta a pagar.*/
-		
-		/*System.out.println("digite o codigo do item:");
-		int codigo = sc.nextInt();
-		System.out.println("digite a quantidade:");
-		int quantidade = sc.nextInt();
-		
-		if (codigo == 1) {
-			System.out.println("Total: R$ " + quantidade*4.00);
-		} else if(codigo == 2) {
-			System.out.println("Total: R$ " + quantidade*4.50);
-		}else if(codigo == 3) {
-			System.out.println("Total: R$ " + quantidade*5.00);
-		}else if(codigo == 4) {
-			System.out.println("Total: R$ " + quantidade*2.00);
-		}else if(codigo == 5) {
-			System.out.println("Total: R$ " + quantidade*1.50);
-		}*/
-		
-		/*Você deve fazer um programa que leia um valor qualquer e apresente uma mensagem dizendo em qual dos
-seguintes intervalos ([0,25], (25,50], (50,75], (75,100]) este valor se encontra. Obviamente se o valor não estiver em
-nenhum destes intervalos, deverá ser impressa a mensagem “Fora de intervalo”.*/
-		
-		/*System.out.println("digite o numero para saber em qual intervalo esta:");
-		double entrada = sc.nextDouble();
-		
-		if (entrada>=0.0) {
-			if (entrada<=25.0) {
-				System.out.println("Intervalo [0,25]\n");
-			}else if (25.0<entrada && entrada<=50.0) {
-				System.out.println("Intervalo [25,50]\n");
-			}else if (50.0<entrada && entrada<75.0) {
-				System.out.println("Intervalo [50,75]\n");
-			}else if (75.0<entrada && entrada<=100.0) {
-				System.out.println("Intervalo [75,100]\n");
-			}
-		}else {
-			System.out.println("Fora de intervalo");
-		}*/
-		
-		/*Leia 2 valores com uma casa decimal (x e y), que devem representar as coordenadas
-de um ponto em um plano. A seguir, determine qual o quadrante ao qual pertence o
-ponto, ou se está sobre um dos eixos cartesianos ou na origem (x = y = 0).
-Se o ponto estiver na origem, escreva a mensagem “Origem”.
-Se o ponto estiver sobre um dos eixos escreva “Eixo X” ou “Eixo Y”, conforme for a
-situação.*/
-		
-		/*System.out.println("digite o valor de x:");
-		double x = sc.nextDouble();
-		System.out.println("digite o valor de y:");
-		double y = sc.nextDouble();
-		
-		if (x == 0.0 && y == 0.0) {
-			System.out.println("Origem");
-		}
-		else if (x == 0.0) {
-			System.out.println("Eixo Y");
-		}
-		else if (y == 0.0) {
-			System.out.println("Eixo X");
-		}
-		else if (x > 0.0 && y > 0.0) {
-			System.out.println("Q1");
-		}
-		else if (x < 0.0 && y > 0.0) {
-			System.out.println("Q2");
-		}
-		else if (x < 0.0 && y < 0.0) {
-			System.out.println("Q3");
-		}
-		else {
-			System.out.println("Q4");
-		}*/
-		
-		/*Em um país imaginário denominado Lisarb, todos os habitantes ficam felizes em pagar seus impostos, pois sabem
-que nele não existem políticos corruptos e os recursos arrecadados são utilizados em benefício da população, sem
-qualquer desvio. A moeda deste país é o Rombus, cujo símbolo é o R$.
-Leia um valor com duas casas decimais, equivalente ao salário de uma pessoa de Lisarb. Em seguida, calcule e
-mostre o valor que esta pessoa deve pagar de Imposto de Renda, segundo a tabela abaixo.
-Lembre que, se o salário for R$ 3002.00, a taxa que incide é de 8% apenas sobre R$ 1000.00, pois a faixa de
-salário que fica de R$ 0.00 até R$ 2000.00 é isenta de Imposto de Renda. No exemplo fornecido (abaixo), a taxa é
-de 8% sobre R$ 1000.00 + 18% sobre R$ 2.00, o que resulta em R$ 80.36 no total. O valor deve ser impresso com
-duas casas decimais.*/
-		
-		/*System.out.println("digite seu salario para calcular o imposto:");
-		double salario = sc.nextDouble();
-		
-		double calculoImposto = 0.0;
-		
-		if (salario<=2000.00) {
-			calculoImposto = 0.0;
-		}else if (salario>= 2000.01 && salario<=3000.00) {
-			calculoImposto =(salario - 2000.0) * 0.08;
-		}else if (salario>= 3000.01 && salario<=4500.00) {
-			calculoImposto = (salario - 3000.0) * 0.18 + 1000.0 * 0.08;
-		}else if (salario> 4500.00) {
-			calculoImposto =  (salario - 4500.0) * 0.28 + 1500.0 * 0.18 + 1000.0 * 0.08;
-		}
-		if (calculoImposto == 0.0) {
-			System.out.println("Isento");
-		}
-		else {
-			System.out.printf("R$ %.2f%n", calculoImposto);
-		}*/
+		// Fazer um programa para ler um número inteiro, e depois dizer se este número é
+		// negativo ou não.
+
+		/*
+		 * System.out.println("digite o primeiro numero inteiro:"); int numero =
+		 * sc.nextInt(); if (numero<0) System.out.println("NEGATIVO"); else
+		 * System.out.println("NAO NEGATIVO");
+		 */
+
+		// Fazer um programa para ler um número inteiro e dizer se este número é par ou
+		// ímpar.
+
+		/*
+		 * System.out.println("digite o primeiro numero inteiro:"); int numero =
+		 * sc.nextInt(); if (numero%2==0) System.out.println("PAR"); else
+		 * System.out.println("IMPAR");
+		 */
+
+		/*
+		 * Leia 2 valores inteiros (A e B). Após, o programa deve mostrar uma mensagem
+		 * "Sao Multiplos" ou "Nao sao Multiplos", indicando se os valores lidos são
+		 * múltiplos entre si. Atenção: os números devem poder ser digitados em ordem
+		 * crescente ou decrescente
+		 */
+
+		/*
+		 * System.out.println("digite o primeiro numero inteiro:"); int numero1 =
+		 * sc.nextInt(); System.out.println("digite o segundo numero inteiro:"); int
+		 * numero2 = sc.nextInt();
+		 * 
+		 * if (numero2%numero1 == 0 || numero1%numero2 == 0)
+		 * System.out.println("Sao Multiplos"); else
+		 * System.out.println("Nao sao Multiplos");
+		 */
+
+		/*
+		 * Leia a hora inicial e a hora final de um jogo. A seguir calcule a duração do
+		 * jogo, sabendo que o mesmo pode começar em um dia e terminar em outro, tendo
+		 * uma duração mínima de 1 hora e máxima de 24 horas.
+		 */
+
+		/*
+		 * System.out.println("digite a hora inicial do jogo:"); int horaInicial =
+		 * sc.nextInt(); System.out.println("digite a hora final do jogo:"); int
+		 * horaFinal = sc.nextInt(); int calculoHora; if (horaInicial < horaFinal)
+		 * calculoHora = horaFinal - horaInicial; else calculoHora = 24 - horaInicial +
+		 * horaFinal;
+		 * 
+		 * System.out.println("O JOGO DUROU " + calculoHora + " HORA(S)");
+		 */
+
+		/*
+		 * Com base na tabela abaixo, escreva um programa que leia o código de um item e
+		 * a quantidade deste item. A seguir, calcule e mostre o valor da conta a pagar.
+		 */
+
+		/*
+		 * System.out.println("digite o codigo do item:"); int codigo = sc.nextInt();
+		 * System.out.println("digite a quantidade:"); int quantidade = sc.nextInt();
+		 * 
+		 * if (codigo == 1) { System.out.println("Total: R$ " + quantidade*4.00); } else
+		 * if(codigo == 2) { System.out.println("Total: R$ " + quantidade*4.50); }else
+		 * if(codigo == 3) { System.out.println("Total: R$ " + quantidade*5.00); }else
+		 * if(codigo == 4) { System.out.println("Total: R$ " + quantidade*2.00); }else
+		 * if(codigo == 5) { System.out.println("Total: R$ " + quantidade*1.50); }
+		 */
+
+		/*
+		 * Você deve fazer um programa que leia um valor qualquer e apresente uma
+		 * mensagem dizendo em qual dos seguintes intervalos ([0,25], (25,50], (50,75],
+		 * (75,100]) este valor se encontra. Obviamente se o valor não estiver em nenhum
+		 * destes intervalos, deverá ser impressa a mensagem “Fora de intervalo”.
+		 */
+
+		/*
+		 * System.out.println("digite o numero para saber em qual intervalo esta:");
+		 * double entrada = sc.nextDouble();
+		 * 
+		 * if (entrada>=0.0) { if (entrada<=25.0) {
+		 * System.out.println("Intervalo [0,25]\n"); }else if (25.0<entrada &&
+		 * entrada<=50.0) { System.out.println("Intervalo [25,50]\n"); }else if
+		 * (50.0<entrada && entrada<75.0) { System.out.println("Intervalo [50,75]\n");
+		 * }else if (75.0<entrada && entrada<=100.0) {
+		 * System.out.println("Intervalo [75,100]\n"); } }else {
+		 * System.out.println("Fora de intervalo"); }
+		 */
+
+		/*
+		 * Leia 2 valores com uma casa decimal (x e y), que devem representar as
+		 * coordenadas de um ponto em um plano. A seguir, determine qual o quadrante ao
+		 * qual pertence o ponto, ou se está sobre um dos eixos cartesianos ou na origem
+		 * (x = y = 0). Se o ponto estiver na origem, escreva a mensagem “Origem”. Se o
+		 * ponto estiver sobre um dos eixos escreva “Eixo X” ou “Eixo Y”, conforme for a
+		 * situação.
+		 */
+
+		/*
+		 * System.out.println("digite o valor de x:"); double x = sc.nextDouble();
+		 * System.out.println("digite o valor de y:"); double y = sc.nextDouble();
+		 * 
+		 * if (x == 0.0 && y == 0.0) { System.out.println("Origem"); } else if (x ==
+		 * 0.0) { System.out.println("Eixo Y"); } else if (y == 0.0) {
+		 * System.out.println("Eixo X"); } else if (x > 0.0 && y > 0.0) {
+		 * System.out.println("Q1"); } else if (x < 0.0 && y > 0.0) {
+		 * System.out.println("Q2"); } else if (x < 0.0 && y < 0.0) {
+		 * System.out.println("Q3"); } else { System.out.println("Q4"); }
+		 */
+
+		/*
+		 * Em um país imaginário denominado Lisarb, todos os habitantes ficam felizes em
+		 * pagar seus impostos, pois sabem que nele não existem políticos corruptos e os
+		 * recursos arrecadados são utilizados em benefício da população, sem qualquer
+		 * desvio. A moeda deste país é o Rombus, cujo símbolo é o R$. Leia um valor com
+		 * duas casas decimais, equivalente ao salário de uma pessoa de Lisarb. Em
+		 * seguida, calcule e mostre o valor que esta pessoa deve pagar de Imposto de
+		 * Renda, segundo a tabela abaixo. Lembre que, se o salário for R$ 3002.00, a
+		 * taxa que incide é de 8% apenas sobre R$ 1000.00, pois a faixa de salário que
+		 * fica de R$ 0.00 até R$ 2000.00 é isenta de Imposto de Renda. No exemplo
+		 * fornecido (abaixo), a taxa é de 8% sobre R$ 1000.00 + 18% sobre R$ 2.00, o
+		 * que resulta em R$ 80.36 no total. O valor deve ser impresso com duas casas
+		 * decimais.
+		 */
+
+		/*
+		 * System.out.println("digite seu salario para calcular o imposto:"); double
+		 * salario = sc.nextDouble();
+		 * 
+		 * double calculoImposto = 0.0;
+		 * 
+		 * if (salario<=2000.00) { calculoImposto = 0.0; }else if (salario>= 2000.01 &&
+		 * salario<=3000.00) { calculoImposto =(salario - 2000.0) * 0.08; }else if
+		 * (salario>= 3000.01 && salario<=4500.00) { calculoImposto = (salario - 3000.0)
+		 * * 0.18 + 1000.0 * 0.08; }else if (salario> 4500.00) { calculoImposto =
+		 * (salario - 4500.0) * 0.28 + 1500.0 * 0.18 + 1000.0 * 0.08; } if
+		 * (calculoImposto == 0.0) { System.out.println("Isento"); } else {
+		 * System.out.printf("R$ %.2f%n", calculoImposto); }
+		 */
 		sc.close();
 	}
 
--- curso_de_java/src/conceitoProgramacao/EstruturaSequencial.java
+++ curso_de_java/src/conceitoProgramacao/EstruturaSequencial.java
@@ -5,107 +5,113 @@ import java.util.Scanner;
 public class EstruturaSequencial {
 
 	public static void main(String[] args) {
-Scanner sc = new Scanner(System.in);
-		
-		/*Faça um programa para ler dois valores inteiros, e depois mostrar na tela a soma desses números com uma
-mensagem explicativa, conforme exemplos.*/
-		
-		/*System.out.println("digite o primeiro numero:");
-		int numero1 = sc.nextInt(); 
-		System.out.println("digite o segundo numero:");
-		int numero2 = sc.nextInt(); 
-		int resutado = numero1 + numero2;
-		System.out.printf("o resultado da soma dos numeros %d e %d é igual a %d",numero1, numero2, resutado);*/
-		
-		/*Faça um programa para ler o valor do raio de um círculo, e depois mostrar o valor da área deste círculo com quatro
-casas decimais conforme exemplos.*/
-		
-		/*System.out.println("programa para ler o valor do raio de um círculo");
-		System.out.println("digite o valor do raio:");
-		double raio = sc.nextDouble(); 
-		double pi = 3.14159;
-		double area = pi*(raio*raio);
-		System.out.printf("o valor da área deste círculo %.4f",area);*/
-		
-		/*Fazer um programa para ler quatro valores inteiros A, B, C e D. A seguir, calcule e mostre a diferença do produto
-de A e B pelo produto de C e D segundo a fórmula: DIFERENCA = (A * B - C * D).*/
-		
-		/*System.out.println("digite o primeiro numero:");
-		int A = sc.nextInt();
-		System.out.println("digite o segundo numero:");
-		int B = sc.nextInt();
-		System.out.println("digite o terceiro numero:");
-		int C = sc.nextInt();
-		System.out.println("digite o quarto numero:");
-		int D = sc.nextInt();
+		Scanner sc = new Scanner(System.in);
 
-		int diferenca =  (A * B - C * D);
-		
-		System.out.printf("a diferença do produto\r\n"
-				+ "de A e B pelo produto de C e D é igual a: %d", diferenca);*/
-		
-		/*Fazer um programa que leia o número de um funcionário, seu número de horas trabalhadas, o valor que recebe por
-hora e calcula o salário desse funcionário. A seguir, mostre o número e o salário do funcionário, com duas casas
-decimais.*/
-		
-		/*System.out.println("digite o numero do funcionario:");
-		double NUMBER = sc.nextDouble();
-		System.out.println("digite o numero de horas trabalhadas:");
-		double horasTrabalhadas = sc.nextDouble();
-		System.out.println("o valor que o funcionario rece por hora:");
-		double recebePorHora = sc.nextDouble();
-		double SALARY = recebePorHora* horasTrabalhadas;
-		System.out.printf("NUMBER = %.0f\r\n"
-				+ "SALARY = U$ %.2f", NUMBER,SALARY);*/
-		
-		/*Fazer um programa para ler o código de uma peça 1, o número de peças 1, o valor unitário de cada peça 1, o
-código de uma peça 2, o número de peças 2 e o valor unitário de cada peça 2. Calcule e mostre o valor a ser pago.*/
-		
-		/*System.out.println("digite o código de uma peça1:");
-		double peca1 = sc.nextDouble();
-		System.out.println("digite o número de peças1:");
-		double numeroDePecas1 = sc.nextDouble();
-		System.out.println("digite o valor unitário de cada peça1:");
-		double  valorUnitario1 = sc.nextDouble();
-		
-		System.out.println("digite o código de uma peça2:");
-		double peca2 = sc.nextDouble();
-		System.out.println("digite o número de peças2:");
-		double numeroDePecas2 = sc.nextDouble();
-		System.out.println("digite o valor unitário de cada peça2:");
-		double  valorUnitario2 = sc.nextDouble();
-		
-		double calculo1 = numeroDePecas1*valorUnitario1;
-		double calculo2 = numeroDePecas2*valorUnitario2;
-		double resutado = calculo1+calculo2;
-		System.out.printf("VALOR A PAGAR: R$ %.2f", resutado);*/
-		
-		/*Fazer um programa que leia três valores com ponto flutuante de dupla precisão: A, B e C. Em seguida, calcule e
-mostre:
-a) a área do triângulo retângulo que tem A por base e C por altura.
-b) a área do círculo de raio C. (pi = 3.14159)
-c) a área do trapézio que tem A e B por bases e C por altura.
-d) a área do quadrado que tem lado B.
-e) a área do retângulo que tem lados A e B.*/
-		
-		System.out.println("digite o primeiro numero:");
-		double A = sc.nextDouble();
-		System.out.println("digite o segundo numero:");
-		double B = sc.nextDouble();
-		System.out.println("digite o terceiro numero:");
-		double C = sc.nextDouble();
-		
-		double pi = 3.14159;
-		double TRIANGULO = (A*C)/2;
-		double CIRCULO = pi*(C*C);
-		double TRAPEZIO = ((A+B)*C)/2;
-		double QUADRADO = B*B; 
-		double RETANGULO = B*A;
-		
-		System.out.printf("TRIANGULO: %.3f\r\n"
-				+ "CIRCULO: %.3f\r\n"
-				+ "TRAPEZIO: %.3f\r\n"
-				+ "QUADRADO: %.3f\r\n"
-				+ "RETANGULO: %.3f",TRIANGULO, CIRCULO, TRAPEZIO, QUADRADO, RETANGULO);
+		/*
+		 * Faça um programa para ler dois valores inteiros, e depois mostrar na tela a
+		 * soma desses números com uma mensagem explicativa, conforme exemplos.
+		 */
+
+		/*
+		 * System.out.println("digite o primeiro numero:"); int numero1 = sc.nextInt();
+		 * System.out.println("digite o segundo numero:"); int numero2 = sc.nextInt();
+		 * int resutado = numero1 + numero2;
+		 * System.out.printf("o resultado da soma dos numeros %d e %d é igual a %d"
+		 * ,numero1, numero2, resutado);
+		 */
+
+		/*
+		 * Faça um programa para ler o valor do raio de um círculo, e depois mostrar o
+		 * valor da área deste círculo com quatro casas decimais conforme exemplos.
+		 */
+
+		/*
+		 * System.out.println("programa para ler o valor do raio de um círculo");
+		 * System.out.println("digite o valor do raio:"); double raio = sc.nextDouble();
+		 * double pi = 3.14159; double area = pi*(raio*raio);
+		 * System.out.printf("o valor da área deste círculo %.4f",area);
+		 */
+
+		/*
+		 * Fazer um programa para ler quatro valores inteiros A, B, C e D. A seguir,
+		 * calcule e mostre a diferença do produto de A e B pelo produto de C e D
+		 * segundo a fórmula: DIFERENCA = (A * B - C * D).
+		 */
+
+		/*
+		 * System.out.println("digite o primeiro numero:"); int A = sc.nextInt();
+		 * System.out.println("digite o segundo numero:"); int B = sc.nextInt();
+		 * System.out.println("digite o terceiro numero:"); int C = sc.nextInt();
+		 * System.out.println("digite o quarto numero:"); int D = sc.nextInt();
+		 * 
+		 * int diferenca = (A * B - C * D);
+		 * 
+		 * System.out.printf("a diferença do produto\r\n" +
+		 * "de A e B pelo produto de C e D é igual a: %d", diferenca);
+		 */
+
+		/*
+		 * Fazer um programa que leia o número de um funcionário, seu número de horas
+		 * trabalhadas, o valor que recebe por hora e calcula o salário desse
+		 * funcionário. A seguir, mostre o número e o salário do funcionário, com duas
+		 * casas decimais.
+		 */
+
+		/*
+		 * System.out.println("digite o numero do funcionario:"); double NUMBER =
+		 * sc.nextDouble(); System.out.println("digite o numero de horas trabalhadas:");
+		 * double horasTrabalhadas = sc.nextDouble();
+		 * System.out.println("o valor que o funcionario rece por hora:"); double
+		 * recebePorHora = sc.nextDouble(); double SALARY = recebePorHora*
+		 * horasTrabalhadas; System.out.printf("NUMBER = %.0f\r\n" + "SALARY = U$ %.2f",
+		 * NUMBER,SALARY);
+		 */
+
+		/*
+		 * Fazer um programa para ler o código de uma peça 1, o número de peças 1, o
+		 * valor unitário de cada peça 1, o código de uma peça 2, o número de peças 2 e
+		 * o valor unitário de cada peça 2. Calcule e mostre o valor a ser pago.
+		 */
+
+		/*
+		 * System.out.println("digite o código de uma peça1:"); double peca1 =
+		 * sc.nextDouble(); System.out.println("digite o número de peças1:"); double
+		 * numeroDePecas1 = sc.nextDouble();
+		 * System.out.println("digite o valor unitário de cada peça1:"); double
+		 * valorUnitario1 = sc.nextDouble();
+		 * 
+		 * System.out.println("digite o código de uma peça2:"); double peca2 =
+		 * sc.nextDouble(); System.out.println("digite o número de peças2:"); double
+		 * numeroDePecas2 = sc.nextDouble();
+		 * System.out.println("digite o valor unitário de cada peça2:"); double
+		 * valorUnitario2 = sc.nextDouble();
+		 * 
+		 * double calculo1 = numeroDePecas1*valorUnitario1; double calculo2 =
+		 * numeroDePecas2*valorUnitario2; double resutado = calculo1+calculo2;
+		 * System.out.printf("VALOR A PAGAR: R$ %.2f", resutado);
+		 */
+
+		/*
+		 * Fazer um programa que leia três valores com ponto flutuante de dupla
+		 * precisão: A, B e C. Em seguida, calcule e mostre: a) a área do triângulo
+		 * retângulo que tem A por base e C por altura. b) a área do círculo de raio C.
+		 * (pi = 3.14159) c) a área do trapézio que tem A e B por bases e C por altura.
+		 * d) a área do quadrado que tem lado B. e) a área do retângulo que tem lados A
+		 * e B.
+		 */
+
+		/*
+		 * System.out.println("digite o primeiro numero:"); double A = sc.nextDouble();
+		 * System.out.println("digite o segundo numero:"); double B = sc.nextDouble();
+		 * System.out.println("digite o terceiro numero:"); double C = sc.nextDouble();
+		 * 
+		 * double pi = 3.14159; double TRIANGULO = (A * C) / 2; double CIRCULO = pi * (C
+		 * * C); double TRAPEZIO = ((A + B) * C) / 2; double QUADRADO = B * B; double
+		 * RETANGULO = B * A;
+		 * 
+		 * System.out.printf("TRIANGULO: %.3f\r\n" + "CIRCULO: %.3f\r\n" +
+		 * "TRAPEZIO: %.3f\r\n" + "QUADRADO: %.3f\r\n" + "RETANGULO: %.3f", TRIANGULO,
+		 * CIRCULO, TRAPEZIO, QUADRADO, RETANGULO);
+		 */
 	}
 }
