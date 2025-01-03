6e2fbb3010cc7684936611ab92175e37bcd52230
raffael-marinho <silvamarinho10@gmail.com>
2025-01-03 16:42:20 -0300

    feat: add questão 1 do while

--- curso_de_java/src/conceitoProgramacao/EstruturaRepetitivaWhile.java
+++ curso_de_java/src/conceitoProgramacao/EstruturaRepetitivaWhile.java
@@ -1,10 +1,46 @@
 package conceitoProgramacao;
 
+import java.util.Scanner;
+
 public class EstruturaRepetitivaWhile {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		/*
+		 * Escreva um programa que repita a leitura de uma senha até que ela seja
+		 * válida. Para cada leitura de senha incorreta informada, escrever a mensagem
+		 * "Senha Invalida". Quando a senha for informada corretamente deve ser impressa
+		 * a mensagem "Acesso Permitido" e o algoritmo encerrado. Considere que a senha
+		 * correta é o valor 2002.
+		 */
+		System.out.println("digite a senha:");
+		int senha = sc.nextInt();
 
+		while (senha != 2002) {
+			System.out.println("Senha Invalida");
+			senha = sc.nextInt();
+		}
+		System.out.println("Acesso Permitido");
+
+		/*
+		 * Escreva um programa para ler as coordenadas (X,Y) de uma quantidade
+		 * indeterminada de pontos no sistema cartesiano. Para cada ponto escrever o
+		 * quadrante a que ele pertence. O algoritmo será encerrado quando pelo menos
+		 * uma de duas coordenadas for NULA (nesta situação sem escrever mensagem
+		 * alguma).
+		 */
+
+		/*
+		 * Um Posto de combustíveis deseja determinar qual de seus produtos tem a
+		 * preferência de seus clientes. Escreva um algoritmo para ler o tipo de
+		 * combustível abastecido (codificado da seguinte forma: 1.Álcool 2.Gasolina
+		 * 3.Diesel 4.Fim). Caso o usuário informe um código inválido (fora da faixa de
+		 * 1 a 4) deve ser solicitado um novo código (até que seja válido). O programa
+		 * será encerrado quando o código informado for o número 4. Deve ser escrito a
+		 * mensagem: "MUITO OBRIGADO" e a quantidade de clientes que abasteceram cada
+		 * tipo de combustível, conforme exemplo.
+		 */
+		sc.close();
 	}
 
 }
